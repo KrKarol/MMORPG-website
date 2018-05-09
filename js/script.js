e66ba09ac4ffe3d61449764137b2002c02eb6ab8
KrKarol <38439332+KrKarol@users.noreply.github.com>
2018-05-09 20:55:11 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("nav"),
+    ul = document.getElementById("navi");
+
+
+nav.addEventListener("click",function(){
+    
+        ul.classList.toggle("menu")
+        nav.classList.toggle("is-active")
+    
+}, false);
+
+
+
+
+
+
